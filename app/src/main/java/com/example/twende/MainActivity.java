467252a9ea07ec46d0692cb95e98eacf5cddc291
Arunda Victor <victoroyugi31@gmail.com>
2019-02-28 11:25:00 +0300

    add click listeners

--- app/src/main/java/com/example/twende/MainActivity.java
+++ app/src/main/java/com/example/twende/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.twende;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //welcome button
+    private Button mWelcomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //declaring button
+        mWelcomeButton = (Button) findViewById(R.id.welcomeButton);
     }
 }
