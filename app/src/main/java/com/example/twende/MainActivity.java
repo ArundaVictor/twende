ad4f7cd5f75832dbe99b7194361441d57e84e9ca
Arunda Victor <victoroyugi31@gmail.com>
2019-02-28 11:33:18 +0300

    add toast

--- app/src/main/java/com/example/twende/MainActivity.java
+++ app/src/main/java/com/example/twende/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.twende;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //welcome button
@@ -12,8 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         //declaring button
         mWelcomeButton = (Button) findViewById(R.id.welcomeButton);
+
+        //click listener for button
+        mWelcomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //add a toast pop up above the button
+                Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
