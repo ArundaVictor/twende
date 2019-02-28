244a036dc3575c16bab263804d92f106fb66e065
Arunda Victor <victoroyugi31@gmail.com>
2019-02-28 13:05:08 +0300

    add new method

--- app/src/main/java/com/example/twende/WelcomeActivity.java
+++ app/src/main/java/com/example/twende/WelcomeActivity.java
@@ -2,12 +2,23 @@ package com.example.twende;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Button  mEventsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        mEventsButton = (Button) findViewById(R.id.eventsButton);
+        mEventsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
