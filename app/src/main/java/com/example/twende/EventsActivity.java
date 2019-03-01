1cc3409200d461c7016f68cf906550c0420983e7
Arunda Victor <victoroyugi31@gmail.com>
2019-03-01 11:40:01 +0300

    add an array adapter'

--- app/src/main/java/com/example/twende/EventsActivity.java
+++ app/src/main/java/com/example/twende/EventsActivity.java
@@ -3,6 +3,7 @@ package com.example.twende;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -20,6 +21,11 @@ public class EventsActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.listView);
         mTextView = (TextView) findViewById(R.id.textView);
 
+        //array adapter to display lists
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, events);
+        mListView.setAdapter(adapter);
+
+        //gathering data from intent extras
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mTextView.setText("Here are all the events happening in " + location);
