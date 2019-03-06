ea26c586feeb6601fc08839235ec49d83578b1e2
Arunda Victor <victoroyugi31@gmail.com>
2019-03-06 16:31:59 +0300

    add call request

--- app/src/main/java/com/example/twende/Constants.java
+++ app/src/main/java/com/example/twende/Constants.java
@@ -1,7 +1,7 @@
 package com.example.twende;
 
 public class Constants {
-    public static final String EventToken = BuildConfig.EventToken;
+    public static final String EVENT_TOKEN = BuildConfig.EVENT_TOKEN;
     public static final String EVENT_BASE_URL = "https://www.eventbriteapi.com/v3/events/search";
     public static final String EVENT_LOCATION_QUERY_PARAMETER = "location.address";
 }
--- app/src/main/java/com/example/twende/EventBriteService.java
+++ app/src/main/java/com/example/twende/EventBriteService.java
@@ -1,13 +1,31 @@
 package com.example.twende;
 
+import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class EventBriteService {
 
     public static void findEvents (String location, Callback callback){
 
+        //client to create and send request
         OkHttpClient client = new OkHttpClient.Builder()
                 .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.EVENT_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.EVENT_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", Constants.EVENT_TOKEN)
+                .build();
+
+        //calling a request Asynchronously
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+
     }
 }
