e8b10f295a963f99a76a59b127210822b43c3959
Arunda Victor <victoroyugi31@gmail.com>
2019-03-06 17:17:17 +0300

    add callback method

--- app/src/main/java/com/example/twende/EventsActivity.java
+++ app/src/main/java/com/example/twende/EventsActivity.java
@@ -1,17 +1,25 @@
 package com.example.twende;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class EventsActivity extends AppCompatActivity {
+    public static final String TAG = EventsActivity.class.getSimpleName();
    @BindView(R.id.listView) ListView mListView;
     @BindView(R.id.textView) TextView mTextView;
     private String[] events = new String[] {"Koroga Festival", "Sun Glasses",
@@ -24,6 +32,8 @@ public class EventsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_events);
         ButterKnife.bind(this);
 
+
+
         //array adapter to display lists
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, events);
         mListView.setAdapter(adapter);
@@ -33,5 +43,36 @@ public class EventsActivity extends AppCompatActivity {
         String location = intent.getStringExtra("location");
         mTextView.setText("Here are all the events happening in " + location);
 
+        getEvents(location);
+
+
+
+    }
+
+    //callback method
+    private void getEvents(String location){
+
+        final EventBriteService eventBriteService = new EventBriteService();
+        eventBriteService.findEvents(location, new Callback() {
+
+            //triggered when our request fails
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+
+            }
+
+            //triggered when request is successful
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
