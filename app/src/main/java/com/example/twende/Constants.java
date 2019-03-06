dcfafd3e339d66a16344544e40aa85b54bb4e46e
Arunda Victor <victoroyugi31@gmail.com>
2019-03-06 13:19:44 +0300

    add new client

--- app/src/main/java/com/example/twende/Constants.java
+++ app/src/main/java/com/example/twende/Constants.java
@@ -2,4 +2,6 @@ package com.example.twende;
 
 public class Constants {
     public static final String EventToken = BuildConfig.EventToken;
+    public static final String EVENT_BASE_URL = "https://www.eventbriteapi.com/v3/events/search";
+    public static final String EVENT_LOCATION_QUERY_PARAMETER = "location.address";
 }
---
+++ app/src/main/java/com/example/twende/EventBriteService.java
@@ -0,0 +1,13 @@
+package com.example.twende;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+
+public class EventBriteService {
+
+    public static void findEvents (String location, Callback callback){
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+    }
+}
